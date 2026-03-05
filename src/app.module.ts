4b0d62e84f8498a425cec292d8efbb5fed807cc4
jirehfather <jirehfather@gmail.com>
2026-03-05 23:00:04 +0900

    🔨Update: enhance MikroORM migration logging and restore resource management logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,21 +15,28 @@ export class AppModule {
   constructor(private readonly orm: MikroORM) {}
 
   async onModuleInit(): Promise<void> {
-    await this.orm.migrator.up();
+    this.logger.log(`Start MikroORM Migration...`);
+    try {
+      await this.orm.migrator.up();
+    } catch (error) {
+      this.logger.error(`MikroORM Migration Failed.`, error);
+      throw error;
+    }
+    this.logger.log(`MikroORM Migration Completed.`);
   }
 
   async onApplicationShutdown(signal?: string) {
     this.logger.log(`Start Shut Down Graceful with ${signal}`);
-    // await Promise.resolve().then(async () => {
-    //   this.logger.log('Try Resources Close...');
+    await Promise.resolve().then(async () => {
+      this.logger.log('Try Resources Close...');
 
-    //   const isConnected = await this.orm.isConnected();
-    //   if (isConnected) {
-    //     this.logger.log('Start MikroORM Close...');
-    //     await this.orm.close();
-    //   }
+      const isConnected = await this.orm.isConnected();
+      if (isConnected) {
+        this.logger.log('Start MikroORM Close...');
+        await this.orm.close();
+      }
 
-    //   this.logger.log('Finish Resources Close...');
-    // });
+      this.logger.log('Finish Resources Close...');
+    });
   }
 }
