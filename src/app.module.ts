f58006bf3f98d7d3b79eac1031c68207d502c52d
jirehfather <jirehfather@gmail.com>
2026-03-05 22:49:37 +0900

    🔨Update: comment out migration and shutdown resource management logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,21 +15,21 @@ export class AppModule {
   constructor(private readonly orm: MikroORM) {}
 
   async onModuleInit(): Promise<void> {
-    await this.orm.migrator.up();
+    // await this.orm.migrator.up();
   }
 
   async onApplicationShutdown(signal?: string) {
     this.logger.log(`Start Shut Down Graceful with ${signal}`);
-    await Promise.resolve().then(async () => {
-      this.logger.log('Try Resources Close...');
+    // await Promise.resolve().then(async () => {
+    //   this.logger.log('Try Resources Close...');
 
-      const isConnected = await this.orm.isConnected();
-      if (isConnected) {
-        this.logger.log('Start MikroORM Close...');
-        await this.orm.close();
-      }
+    //   const isConnected = await this.orm.isConnected();
+    //   if (isConnected) {
+    //     this.logger.log('Start MikroORM Close...');
+    //     await this.orm.close();
+    //   }
 
-      this.logger.log('Finish Resources Close...');
-    });
+    //   this.logger.log('Finish Resources Close...');
+    // });
   }
 }
