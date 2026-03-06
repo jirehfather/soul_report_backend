bba9ca42d4866a898f8392b74f71ccc816e9ad29
jirehfather <jirehfather@gmail.com>
2026-03-06 10:16:57 +0900

    🔨Update: refactor getHello method to return user-specific greeting and comment out migration logic

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
+import { EntityManager, MikroORM } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
+import { User } from './lib/entities/user.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly em: EntityManager) {}
+
+  async getHello(): Promise<string> {
+    const user = await this.em.findOne(User, { email: '123' });
+    if (user === null) {
+      return 'User not found';
+    } else {
+      return 'Hello';
+    }
   }
 }
