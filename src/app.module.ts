8a2fab6cc9f6ef2facf59ebb9d621e9bdabce076
jirehfather <jirehfather@gmail.com>
2026-03-06 10:04:40 +0900

    Refactor MikroORM initialization and migration handling
    - Removed MikroORM migration logic from AppModule and moved it to CoreModule.
    - Added logging for migration start and completion in CoreModule.
    - Implemented graceful shutdown for MikroORM connections in CoreModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,42 +1,11 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
-import { MikroORM } from '@mikro-orm/postgresql';
 
 @Module({
   imports: [CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  private readonly logger: Logger = new Logger(AppModule.name);
-
-  constructor(private readonly orm: MikroORM) {}
-
-  async onModuleInit(): Promise<void> {
-    this.logger.log(`Start MikroORM Migration...`);
-    try {
-      await this.orm.migrator.up();
-    } catch (error) {
-      this.logger.error(`MikroORM Migration Failed.`, error);
-      throw error;
-    }
-    this.logger.log(`MikroORM Migration Completed.`);
-  }
-
-  async onApplicationShutdown(signal?: string) {
-    this.logger.log(`Start Shut Down Graceful with ${signal}`);
-    await Promise.resolve().then(async () => {
-      this.logger.log('Try Resources Close...');
-
-      const isConnected = await this.orm.isConnected();
-      if (isConnected) {
-        this.logger.log('Start MikroORM Close...');
-        await this.orm.close();
-      }
-
-      this.logger.log('Finish Resources Close...');
-    });
-  }
-}
+export class AppModule {}
--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,39 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { EnvModule } from './env/env.module';
 import { MikroModule } from './database/mikro.module';
+import { MikroORM } from '@mikro-orm/postgresql';
 
 @Module({
   imports: [EnvModule, MikroModule.forRoot()],
 })
-export class CoreModule {}
+export class CoreModule {
+  private readonly logger: Logger = new Logger(CoreModule.name);
+
+  constructor(private readonly orm: MikroORM) {}
+
+  async onModuleInit(): Promise<void> {
+    this.logger.log(`Start MikroORM Migration...`);
+    try {
+      await this.orm.migrator.up();
+    } catch (error) {
+      this.logger.error(`MikroORM Migration Failed.`, error);
+      throw error;
+    }
+    this.logger.log(`MikroORM Migration Completed.`);
+  }
+
+  async onApplicationShutdown(signal?: string) {
+    this.logger.log(`Start Shut Down Graceful with ${signal}`);
+    await Promise.resolve().then(async () => {
+      this.logger.log('Try Resources Close...');
+
+      const isConnected = await this.orm.isConnected();
+      if (isConnected) {
+        this.logger.log('Start MikroORM Close...');
+        await this.orm.close();
+      }
+
+      this.logger.log('Finish Resources Close...');
+    });
+  }
+}
